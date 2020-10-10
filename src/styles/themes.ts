811ee0856237783f81e597fe325666baa588f54d
Gean Lucas <gean.lfpsa@gmail.com>
2020-10-10 15:45:31 -0300

    Created themes

---
+++ src/styles/themes.ts
@@ -0,0 +1,19 @@
+export const firstTheme = {
+  name: 'first',
+  colors: {
+    primary: '#16a085',
+    black: '#ecf0f1',
+    background: '#2c3e50',
+    border: '#2980b9',
+  }
+}
+
+export const secondTheme = {
+  name: 'second',
+  colors: {
+    primary: '#8e44ad',
+    black: '#34495e',
+    background: '#e74c3c',
+    border: '#27ae60',
+  }
+}
